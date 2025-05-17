8f395ac2f5f46430cfcc17728b341e43495534fc
MightGuy9172 <manishsingh9172@gmail.com>
2025-05-17 16:49:16 +0530

    Added Day8 project

---
+++ Projects/Day8.py
@@ -0,0 +1,56 @@
+def caesar(text,shift,direction):
+    ans = ""
+    if direction=="decode":
+        shift*=-1
+    for char in text:
+        if char in alphabet:
+            if direction=="encode":
+                position = alphabet.index(char) + shift
+                ans += alphabet[position]
+            elif direction=="decode":
+                position = alphabet.index(char) + shift
+                ans += alphabet[position]
+        else:
+            ans+=char
+    print(f"\n{direction}d text is: {ans}")
+
+
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+start=True
+while start:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift%=26
+    caesar(text,shift,direction)
+
+    ask=input("Type 'yes' to restart else 'no' to end:\n").lower()
+    if(ask=="no"):
+        start=False
+        print("Ended")
