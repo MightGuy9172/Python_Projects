e72d1a3ea69f5548ff541f7647a4a39e29330bc6
MightGuy9172 <manishsingh9172@gmail.com>
2025-05-18 13:10:02 +0530

    Added Day9 project

---
+++ Projects/Day9.py
@@ -0,0 +1,46 @@
+#Secret Bidding
+
+logo ='''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+
+bidders = []
+
+
+def addBid(name, bid):
+    people = {}
+    people["name"] = name
+    people["bid"] = bid
+    bidders.append(people)
+
+
+print("Welcome to Auction program.")
+print(logo)
+isStart = True
+
+while isStart:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    addBid(name, bid)
+    temp = input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
+    if temp == "no":
+        isStart = False
+
+max = 0
+winner = ""
+for people in bidders:
+    if people["bid"] > max:
+        max = people["bid"]
+        winner = people["name"]
+
+print(f"The Winner is {winner} with bid of ${max}")
