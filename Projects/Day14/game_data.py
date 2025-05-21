cc5bd76bdf305836322adcca5502615ba171d319
MightGuy9172 <manishsingh9172@gmail.com>
2025-05-21 11:15:22 +0530

    Added Day14 project

---
+++ Projects/Day14/game_data.py
@@ -0,0 +1,602 @@
+data=[
+    {
+        "name": "Neha Kakkar",
+        "follower_count": 64,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Hrithik Roshan",
+        "follower_count": 157,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 70,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Disha Patani",
+        "follower_count": 77,
+        "description": "Content Creator",
+        "country": "India"
+    },
+    {
+        "name": "Remo D'Souza",
+        "follower_count": 170,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Shikhar Dhawan",
+        "follower_count": 110,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Badshah",
+        "follower_count": 237,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Mallika Dua",
+        "follower_count": 119,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Tanmay Bhat",
+        "follower_count": 238,
+        "description": "Content Creator",
+        "country": "India"
+    },
+    {
+        "name": "Awez Darbar",
+        "follower_count": 227,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Avneet Kaur",
+        "follower_count": 241,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Hardik Pandya",
+        "follower_count": 233,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Shilpa Shetty",
+        "follower_count": 207,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Katrina Kaif",
+        "follower_count": 182,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Janhvi Kapoor",
+        "follower_count": 57,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Nawazuddin Siddiqui",
+        "follower_count": 78,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Mr. Faisu",
+        "follower_count": 35,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Arijit Singh",
+        "follower_count": 146,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Vir Das",
+        "follower_count": 35,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Rajkummar Rao",
+        "follower_count": 168,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "KL Rahul",
+        "follower_count": 68,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Manoj Bajpayee",
+        "follower_count": 193,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Abhishek Bachchan",
+        "follower_count": 31,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Jannat Zubair",
+        "follower_count": 110,
+        "description": "Director",
+        "country": "India"
+    },
+    {
+        "name": "Pankaj Tripathi",
+        "follower_count": 25,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Kenny Sebastian",
+        "follower_count": 175,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Alia Bhatt",
+        "follower_count": 93,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Divyanka Tripathi",
+        "follower_count": 164,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Akshay Kumar",
+        "follower_count": 129,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "MS Dhoni",
+        "follower_count": 228,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "P.V. Sindhu",
+        "follower_count": 59,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Bhumi Pednekar",
+        "follower_count": 192,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Komal Pandey",
+        "follower_count": 3,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Allu Arjun",
+        "follower_count": 223,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Rajinikanth",
+        "follower_count": 30,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Karan Johar",
+        "follower_count": 44,
+        "description": "Content Creator",
+        "country": "India"
+    },
+    {
+        "name": "Kriti Sanon",
+        "follower_count": 178,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Mouni Roy",
+        "follower_count": 181,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Vicky Kaushal",
+        "follower_count": 50,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Anushka Sharma",
+        "follower_count": 228,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Smriti Mandhana",
+        "follower_count": 66,
+        "description": "Content Creator",
+        "country": "India"
+    },
+    {
+        "name": "Ankush Bahuguna",
+        "follower_count": 206,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Ranbir Kapoor",
+        "follower_count": 11,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Sara Ali Khan",
+        "follower_count": 77,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Sonu Sood",
+        "follower_count": 179,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Kusha Kapila",
+        "follower_count": 70,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Rohit Zinjurke",
+        "follower_count": 162,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 28,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Taapsee Pannu",
+        "follower_count": 11,
+        "description": "Singer",
+        "country": "India"
+    },
+    {
+        "name": "Dolly Singh",
+        "follower_count": 206,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "BeerBiceps",
+        "follower_count": 250,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Varun Dhawan",
+        "follower_count": 37,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Zakir Khan",
+        "follower_count": 104,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Ravindra Jadeja",
+        "follower_count": 228,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Aamir Khan",
+        "follower_count": 157,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Nora Fatehi",
+        "follower_count": 50,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Rohit Shetty",
+        "follower_count": 39,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Saloni Gaur",
+        "follower_count": 18,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Deepika Padukone",
+        "follower_count": 156,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Ashish Chanchlani",
+        "follower_count": 38,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Salman Khan",
+        "follower_count": 218,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Amitabh Bachchan",
+        "follower_count": 110,
+        "description": "Singer",
+        "country": "India"
+    },
+    {
+        "name": "Kangana Ranaut",
+        "follower_count": 31,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Sunny Leone",
+        "follower_count": 34,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Jasprit Bumrah",
+        "follower_count": 106,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Shraddha Kapoor",
+        "follower_count": 220,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Biswa Kalyan Rath",
+        "follower_count": 89,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Sachin Tendulkar",
+        "follower_count": 133,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Rashmika Mandanna",
+        "follower_count": 152,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Harnaaz Sandhu",
+        "follower_count": 198,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Rhea Chakraborty",
+        "follower_count": 197,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "CarryMinati",
+        "follower_count": 152,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Ishaan Khatter",
+        "follower_count": 31,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Siddharth Malhotra",
+        "follower_count": 191,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Kapil Sharma",
+        "follower_count": 54,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Sandeep Maheshwari",
+        "follower_count": 124,
+        "description": "Content Creator",
+        "country": "India"
+    },
+    {
+        "name": "Malaika Arora",
+        "follower_count": 211,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Urvashi Rautela",
+        "follower_count": 30,
+        "description": "Director",
+        "country": "India"
+    },
+    {
+        "name": "Niharika NM",
+        "follower_count": 47,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 211,
+        "description": "Stand-up Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Rohit Sharma",
+        "follower_count": 72,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Saina Nehwal",
+        "follower_count": 126,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Tony Kakkar",
+        "follower_count": 201,
+        "description": "YouTuber",
+        "country": "India"
+    },
+    {
+        "name": "Honey Singh",
+        "follower_count": 28,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Rani Rampal",
+        "follower_count": 40,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Yuvraj Singh",
+        "follower_count": 230,
+        "description": "Producer",
+        "country": "India"
+    },
+    {
+        "name": "Neeraj Chopra",
+        "follower_count": 209,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Tiger Shroff",
+        "follower_count": 207,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Guru Randhawa",
+        "follower_count": 146,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Ekta Kapoor",
+        "follower_count": 27,
+        "description": "Motivational Speaker",
+        "country": "India"
+    },
+    {
+        "name": "Ranveer Singh",
+        "follower_count": 182,
+        "description": "Model",
+        "country": "India"
+    },
+    {
+        "name": "Bhuvan Bam",
+        "follower_count": 67,
+        "description": "Comedian",
+        "country": "India"
+    },
+    {
+        "name": "Hina Khan",
+        "follower_count": 182,
+        "description": "Dancer",
+        "country": "India"
+    },
+    {
+        "name": "Siddharth Nigam",
+        "follower_count": 17,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Samantha Ruth Prabhu",
+        "follower_count": 57,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Prajakta Koli",
+        "follower_count": 115,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Tejasswi Prakash",
+        "follower_count": 51,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Kiara Advani",
+        "follower_count": 220,
+        "description": "Influencer",
+        "country": "India"
+    },
+    {
+        "name": "Rubina Dilaik",
+        "follower_count": 81,
+        "description": "Director",
+        "country": "India"
+    },
+    {
+        "name": "Gauahar Khan",
+        "follower_count": 190,
+        "description": "YouTuber",
+        "country": "India"
+    }
+]
