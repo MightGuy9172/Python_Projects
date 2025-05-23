477c08a243ec610774d2a5c0331135436a8fba93
MightGuy9172 <manishsingh9172@gmail.com>
2025-05-23 10:07:48 +0530

    Added Day17 project

---
+++ Projects/Day17/data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Iron Man's real name is Tony Stark.", "answer": "True"},
+    {"text": "Thor's hammer, Mjolnir, can be lifted by anyone.", "answer": "False"},
+    {"text": "Captain America's shield is made of Vibranium.", "answer": "True"},
+    {"text": "Black Panther's real name is T'Challa.", "answer": "True"},
+    {"text": "Hawkeye is known for using a sword as his primary weapon.", "answer": "False"},
+    {"text": "Doctor Strange was originally a neurosurgeon.", "answer": "True"},
+    {"text": "The Infinity Gauntlet has six Infinity Stones.", "answer": "True"},
+    {"text": "Spider-Man was bitten by a radioactive mosquito.", "answer": "False"},
+    {"text": "Loki is Thor's biological brother.", "answer": "False"},
+    {"text": "The Hulk's alter ego is Bruce Banner.", "answer": "True"},
+    {"text": "Thanos first appeared in the Marvel Cinematic Universe in 'Avengers: Endgame'.", "answer": "False"},
+    {"text": "Wanda Maximoff, also known as Scarlet Witch, has reality-warping powers.", "answer": "True"}
+]
